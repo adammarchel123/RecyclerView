4eb019eea1c4ed208bf1210aaaeec07f2dd82498
adammarchel123 <adammarchel123@gmail.com>
2021-04-02 19:32:52 +0700

    finish

--- app/src/main/java/com/example/recyclerview/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -34,12 +34,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImage.setImageResource(images[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data1.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
