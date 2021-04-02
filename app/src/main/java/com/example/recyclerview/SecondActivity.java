679aa57ac9cda89d079e5a7bb0208f620fc5bc0b
adammarchel123 <adammarchel123@gmail.com>
2021-04-02 20:16:00 +0700

    finishbeneran

--- app/src/main/java/com/example/recyclerview/SecondActivity.java
+++ app/src/main/java/com/example/recyclerview/SecondActivity.java
@@ -12,6 +12,9 @@ public class SecondActivity extends AppCompatActivity {
     ImageView mainImageView;
     TextView title, description;
 
+    String data1, data2;
+    int myImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,17 +23,24 @@ public class SecondActivity extends AppCompatActivity {
         mainImageView = findViewById(R.id.mainImageView);
         title = findViewById(R.id.title);
         description = findViewById(R.id.description);
+
+        getData();
+        setData();
     }
 
     private void getData() {
         if (getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")) {
-
+            data1 = getIntent().getStringExtra("data1");
+            data2 = getIntent().getStringExtra("data2");
+            myImage = getIntent().getIntExtra("myImage", 1);
         } else {
-            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
         }
     }
 
     private void setData() {
-
+        title.setText(data1);
+        description.setText(data2);
+        mainImageView.setImageResource(myImage);
     }
 }
