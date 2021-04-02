bc945466c3c1fff2da655e9e69154e55a45af20b
adammarchel123 <adammarchel123@gmail.com>
2021-04-02 20:09:06 +0700

    Part2_7.20

--- app/src/main/java/com/example/recyclerview/SecondActivity.java
+++ app/src/main/java/com/example/recyclerview/SecondActivity.java
@@ -3,12 +3,34 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    ImageView mainImageView;
+    TextView title, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        mainImageView = findViewById(R.id.mainImageView);
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+    }
+
+    private void getData() {
+        if (getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")) {
+
+        } else {
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setData() {
+
     }
 }
